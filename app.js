203c75222060c2efe0a501cf5dede8a538996f8a
aashitachouhan <76626364+aashitachouhan@users.noreply.github.com>
2021-01-14 14:55:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app=express()
+
+app.get("/",(req,res) => {
+    res.send("Hello World");
+})
+
+app.get("/:name",(req,res) =>{
+    let n= req.params.name;
+    res.send(`Hello ${n}`);
+})
+
+app.listen(5000)
